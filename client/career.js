3efd8a18fc5e3e7813cae1da89c215a476c3484e
sashank chavaly <sashank.chavaly@academy.com>
2023-01-18 19:18:49 +0530

    final changes

---
+++ client/career.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Career() {
+  return (
+    <div>
+        <h1>career</h1>
+        <button onClick={()=>window.location.href='/users'}>career</button>
+    </div>
+  )
+}
+
+export default Career;
